823619bb50877e250d882dd70e23b7085f2381fb
github username <github email address>
2018-11-12 17:57:01 -0500

    added forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_secure_password
   has_many :comments
   has_many :images
-  has_many :hashtags, through: :image_tags  
+  has_many :hashtags, through: :image_tags
+
+
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true 
 end
