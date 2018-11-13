e551cb6601ef5092f6bd1f13c0125eb856cf5ccb
github username <github email address>
2018-11-13 15:54:51 -0500

    sessions controller and user form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      log_in_user(@user)
+      redirect_to images_path
+    else
+      flash[:errors] = [ "Username or password didn't match" ]
+      redirect_to new_sessions_path
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to images_path
 end
