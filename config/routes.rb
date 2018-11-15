efe91e2225be0b4da9e1705a2c5a57b68dc0c8da
github username <github email address>
2018-11-14 19:59:59 -0500

    new: do not need to have joint hashtag with picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :imagetags
   resources :hashtags
-  resources :images
+  resources :images  
   resources :comments
   resources :users, only: [:new, :create, :index, :show, :edit, :update]
   resources :sessions, only: [:new, :create]
